cd0336529345c450af6e5aaf0d02ab2b44522d4d
Sandrina <hrafaelmiracle@gmail.com>
2026-04-06 15:02:49 +0700

    Add Talisman security headers

--- service/__init__.py
+++ service/__init__.py
@@ -6,12 +6,16 @@ and SQL database
 """
 import sys
 from flask import Flask
+from flask_talisman import Talisman  # <--- Tambahkan ini
 from service import config
 from service.common import log_handlers
 
 # Create Flask application
 app = Flask(__name__)
 app.config.from_object(config)
+
+# Tambahkan konfigurasi Talisman untuk keamanan header HTTP
+talisman = Talisman(app)  # <--- Tambahkan ini
 
 # Import the routes After the Flask app is created
 # pylint: disable=wrong-import-position, cyclic-import, wrong-import-order
